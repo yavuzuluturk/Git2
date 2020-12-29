daf0fd2ac1733f73d9d26491e0338f9dd4f2a68c
yavuz <yavuzuluturk@gmail.com>
2020-12-29 17:11:00 +0100

    first commit

---
+++ src/com/techproedpackage/firstseleniumclass.java
@@ -0,0 +1,17 @@
+package com.techproedpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstseleniumclass {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Documents\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.de/");
+        driver.manage().window().maximize();
+    }
+}
